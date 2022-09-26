fd7e57373c805fa220c7497a26beef8a175a7153
wellingtonfidalgo <ton.fidalgo@gmail.com>
2022-09-26 18:02:25 -0300

    atualização

--- Assets/Plugins/WebGL/MyPlugin.jslib
+++
@@ -1,8 +0,0 @@
-var MyPlugin = {
-     IsMobile: function()
-     {
-         return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-     }
- };
- 
- mergeInto(LibraryManager.library, MyPlugin);
--- Assets/TestDevice.cs
+++ Assets/TestDevice.cs
@@ -9,55 +9,20 @@ public class TestDevice : MonoBehaviour
     [SerializeField] private GameObject mobile;
     [SerializeField] private GameObject unknow;
 
-    [DllImport("__Internal")]
-    private static extern bool IsMobile();
-
-    public bool isMobile()
-    {
-        #if !UNITY_EDITOR && UNITY_WEBGL
-            return IsMobile();
-        #endif
-        return false;
-    }
-
     // Start is called before the first frame update
     void Start()
     {
-
-        //if (SystemInfo.deviceType == DeviceType.Desktop)
-        //{
-        //    Debug.Log("Desktop Device");
-
-        //    desktop.SetActive(true);
-        //    mobile.SetActive(false);
-        //    unknow.SetActive(false);
-        //}
-
-        //if(SystemInfo.deviceType == DeviceType.Handheld)
-        //{
-        //    Debug.Log("Método 1");
-
-        //    desktop.SetActive(false);
-        //    mobile.SetActive(true);
-        //    unknow.SetActive(false);
-        //}
-
         if (Application.isMobilePlatform)
         {
-            Debug.Log("Método 2");
-
             desktop.SetActive(false);
             mobile.SetActive(true);
             unknow.SetActive(false);
         }
-
-        //if (isMobile())
-        //{
-        //    Debug.Log("Método 3");
-
-        //    desktop.SetActive(false);
-        //    mobile.SetActive(true);
-        //    unknow.SetActive(false);
-        //}
+        else
+        {
+            desktop.SetActive(true);
+            mobile.SetActive(false);
+            unknow.SetActive(false);
+        }
     }
 }
